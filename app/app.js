a6c8ae07a2603e7fe48b46e902a4859991c2ff09
Otoman Nkomanya <otomannkomanya@vimmak.com>
2018-11-22 22:36:38 -0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,10 @@
+import Vue from 'nativescript-vue';
+import App from './components/App';
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer);
+
+new Vue({
+  render: h => h('frame', [h(App)])
+}).$start();
